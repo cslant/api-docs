f2efa0ec6066921ce708b8d84e893045565f2a08
thuankg1752 <thuankg1752@gmail.com>
2025-03-11 21:15:34 +0700

    fix: type of OpenAISidebar

--- src/configs/openApiSidebar.ts
+++ src/configs/openApiSidebar.ts
@@ -1,14 +1,18 @@
-export const OpenApiSidebar = [
+import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
+
+export const OpenApiSidebar: SidebarsConfig = {
+  openApiSidebar: [
     {
-        type: "category",
-        label: "Blog",
-        link: {
-            type: "generated-index",
-            title: "Blog API",
-            description:
-                "CSlant API Documentation uses the OpenAPI Specification to provide a detailed reference for all available endpoints, request and response examples, and more.",
-            slug: "/category/blog-api"
-        },
-        items: require("../../docs/blog/sidebar.js")
-    },
-]
+      type: 'category',
+      label: 'Blog',
+      link: {
+        type: 'generated-index',
+        title: 'Blog API',
+        description:
+          'CSlant API Documentation uses the OpenAPI Specification to provide a detailed reference for all available endpoints, request and response examples, and more.',
+        slug: '/category/blog-api'
+      },
+      items: require('../../docs/blog/sidebar.js')
+    }
+  ]
+};
