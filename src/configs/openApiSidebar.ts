55817bb421eaf4929d9ab40878c30d92e27e28af
Tan Nguyen <tannp27@gmail.com>
2025-03-11 20:30:25 +0700

    feat: update API documentation description in openApiSidebar

--- src/configs/openApiSidebar.ts
+++ src/configs/openApiSidebar.ts
@@ -6,7 +6,7 @@ export const OpenApiSidebar = [
             type: "generated-index",
             title: "Blog API",
             description:
-                "TopDev API is a robust and scalable API built using Laravel, a popular PHP framework known for its elegance and simplicity. The API is designed to provide a wide range of services and follows the principles of REST, ensuring that it's easy to use, stateless, and can be easily integrated into different applications.",
+                "CSlant API Documentation uses the OpenAPI Specification to provide a detailed reference for all available endpoints, request and response examples, and more.",
             slug: "/category/blog-api"
         },
         items: require("../../docs/blog/sidebar.js")
